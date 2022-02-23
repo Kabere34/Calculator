7103733e626249068555f09369b6af8359f4269e
Ivy Kabere <kabereivy@gmail.com>
2022-02-23 15:26:21 +0300

    calculator commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function display(val) {
+  document.getElementById("textval").value += val;
+}
+function getResult() {
+  let x = document.getElementById("textval").value;
+  let y = eval(x);
+  document.getElementById("textval").value = y;
+}
+function clr() {
+  document.getElementById("textval").value = "";
+}
+function back() {}
